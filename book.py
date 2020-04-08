af3559cb12d534488555a8fe1c3c350a7a25fe6d
Ganddalf <oleinik.ms@phystech.edu>
2020-04-09 01:48:24 +0300

    Omagad, it works 2

--- book.py
+++ book.py
@@ -6,5 +6,8 @@ class Book:
     def __str__(self):
         return ' | '.join([str(x) for x in [self.ID, self.name, self.author, self.category, self.year, self.price]])
 
+    def edit(self, *args):
+        self.ID, self.name, self.author, self.category, self.year, self.price = (args)
+
     def get(self):
         return (self.ID, self.name, self.author, self.category, self.year, self.price)
